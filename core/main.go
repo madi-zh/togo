9b06c724bcd610a7ac53eaaa0f4527a3f3f54e2c
Madi Zhanbyrtayev <m.zhanbyrtaev@gmail.com>
2026-02-17 22:36:14 +0500

    more typing

--- core/main.go
+++ core/main.go
@@ -5,75 +5,97 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 	"tasks"
-	"time"
 )
 
-func createTask(taskPtr *string, desPtr *string) *tasks.Task {
-	return &tasks.Task{Title: *taskPtr, Description: *desPtr, Status: tasks.TaskIncomplete, CreatedAt: time.Now()}
-}
-
-func toggleTaskStatus(item *tasks.Task) {
-	if item.Status == tasks.TaskCompleted {
-		item.Status = tasks.TaskIncomplete
-	} else {
-		item.Status = tasks.TaskCompleted
-	}
-}
-
-func processOp(opsPtr *string, id int64, item *tasks.Task, tasksRepo *tasks.TasksRepository) {
-	switch *opsPtr {
-	case "getList":
-		result := tasksRepo.GetList()
-		for _, res := range result {
-			outputToJson(&res)
-		}
-	case "create":
-		result := tasksRepo.Add(item)
-		outputToJson(result)
-	case "delete":
-		tasksRepo.Delete(id)
-	case "update":
-		fmt.Println("update")
-	case "getOne":
-		result := tasksRepo.GetOne(id)
-		outputToJson(result)
-	default:
-		fmt.Println("Available ops are: add, list, delete")
-	}
-
-}
-
-func outputToJson(item *tasks.Task) []byte {
-	itemJson, _ := json.Marshal(item)
-	return itemJson
-}
-
-func outputToJsonList(items []tasks.Task) []byte {
-	itemsJson, _ := json.Marshal(tasks.TaskList{Items: items})
-	return itemsJson
-}
-
-func hello(w http.ResponseWriter, req *http.Request) {
+func getTasks(w http.ResponseWriter, req *http.Request) {
 	dbSession := db.CreateSession()
 	defer db.CloseSession(dbSession)
 	tasksRepo := tasks.InitRepo(dbSession)
 	items := tasksRepo.GetList()
-	res := outputToJsonList(items)
-	fmt.Fprintf(w, string(res))
+	response := map[string]any{
+		"message": "Done",
+		"items":   items,
+	}
+	jsonResponse(w, response, 200)
+}
+
+func createTask(w http.ResponseWriter, req *http.Request) {
+	var task tasks.Task
+	defer req.Body.Close()
+	err := json.NewDecoder(req.Body).Decode(&task)
+
+	if err != nil {
+		jsonError(w, "Something wrong", http.StatusBadRequest)
+		return
+	}
+	dbSession := db.CreateSession()
+	defer db.CloseSession(dbSession)
+	tasksRepo := tasks.InitRepo(dbSession)
+
+	createdTask := tasksRepo.Add(&task)
+	jsonResponse(w, &createdTask, 200)
+}
+
+func getTask(w http.ResponseWriter, req *http.Request) {
+	taskId, err := strconv.ParseInt(req.PathValue("id"), 10, 64)
+	if err != nil {
+		jsonError(w, "Id is not int", http.StatusBadRequest)
+		return
+	}
+	dbSession := db.CreateSession()
+	defer db.CloseSession(dbSession)
+
+	tasksRepo := tasks.InitRepo(dbSession)
+
+	task := tasksRepo.GetOne(taskId)
+	jsonResponse(w, task, 200)
+}
+
+func deleteTask(w http.ResponseWriter, req *http.Request) {
+	taskId, err := strconv.ParseInt(req.PathValue("id"), 10, 64)
+	if err != nil {
+		jsonError(w, "Id is not int", http.StatusBadRequest)
+		return
+	}
+	if taskId < 1 {
+		jsonError(w, "wrong taskid", http.StatusBadRequest)
+		return
+	}
+	dbSession := db.CreateSession()
+	defer db.CloseSession(dbSession)
+
+	tasksRepo := tasks.InitRepo(dbSession)
+
+	tasksRepo.Delete(taskId)
+	jsonResponse(w, nil, 204)
+}
+
+func jsonError(w http.ResponseWriter, message string, status int) {
+	response := map[string]string{
+		"error": message,
+	}
+
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(response)
+}
+
+func jsonResponse(w http.ResponseWriter, body any, status int) {
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(status)
+	if body != nil {
+		json.NewEncoder(w).Encode(body)
+	}
 }
 
 func main() {
-	// opsPtr := flag.String("op", "default", "operation for the todo list")
-	// taskPtr := flag.String("task", "<your task name>", "task name")
-	// deleteId := flag.Int64("id", -1, "Delete Id")
-	// descriptionPtr := flag.String("des", "<your description>", "please provide description")
-	// flag.Parse()
-
-	http.HandleFunc("/", hello)
+	router := http.NewServeMux()
+	router.HandleFunc("GET /tasks/", getTasks)
+	router.HandleFunc("POST /tasks/", createTask)
+	router.HandleFunc("GET /tasks/{id}", getTask)
+	router.HandleFunc("DELETE /tasks/{id}", deleteTask)
 	fmt.Println("Starting server at http://localhost:8090")
-	http.ListenAndServe(":8090", nil)
-	// todoItem1 := createTask(taskPtr, descriptionPtr)
-	// toggleTaskStatus(todoItem1)
-	// processOp(opsPtr, *deleteId, todoItem1, tasksRepo)
+	http.ListenAndServe(":8090", router)
 }
